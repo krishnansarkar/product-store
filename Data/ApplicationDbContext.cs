d658df2ce5261c0e8fa090281a615f36680ede95
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-08-21 22:31:15 -0400

    add ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
