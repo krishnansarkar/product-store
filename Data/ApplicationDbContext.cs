69f44b1f8951f4cc4db796d32d22bde89db41ec9
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-08-22 22:32:55 -0400

    seed Category table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace ProductStore.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Horror", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Comedy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Action", DisplayOrder = 3 }
+            );
+        }
     }
 }
