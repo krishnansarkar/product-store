ead4c0923b3fbcb76eed81f364011286aa193303
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-08-22 22:19:25 -0400

    add Category table to db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Models;
 
 namespace ProductStore.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        DbSet<Category> Categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
