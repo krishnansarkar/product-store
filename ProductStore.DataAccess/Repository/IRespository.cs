ee49e82df45b86111ff658e818f245e3e3fa5ad2
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-09-07 22:28:37 -0400

    add generic repository pattern

---
+++ ProductStore.DataAccess/Repository/IRespository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace ProductStore.DataAccess.Repository
+{
+    public interface IRespository<T> where T : class
+    {
+        T Get(Expression<Func<T, bool>> expression);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ ProductStore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.DataAccess.Data;
+using System.Linq.Expressions;
+
+namespace ProductStore.DataAccess.Repository
+{
+    public class Repository<T> : IRespository<T> where T : class
+    {
+        private ApplicationDbContext _db;
+        private DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(expression);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
