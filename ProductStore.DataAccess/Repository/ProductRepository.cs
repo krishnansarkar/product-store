b3c4b7fface7a5cb6b2f512374cee3314865a254
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-09-09 22:32:20 -0400

    create ProductRepository and configure with UnitOfWork

---
+++ ProductStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductStore.DataAccess.Data;
+using ProductStore.DataAccess.Repository.IRepository;
+using ProductStore.Models;
+
+namespace ProductStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
