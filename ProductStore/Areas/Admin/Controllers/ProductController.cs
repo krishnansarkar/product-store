9f6cb1b5f4ac92e1a3a61bf958ef2b16bd307f6c
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-09-11 22:14:48 -0400

    create Product List page

---
+++ ProductStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStore.DataAccess.Repository.IRepository;
+using ProductStore.Models;
+
+namespace ProductStore.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+
+            return View(products);
+        }
+    }
+}
