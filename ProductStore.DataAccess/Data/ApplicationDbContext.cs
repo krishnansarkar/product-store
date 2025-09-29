bce3f00814f14ce7dfe330c7b38e293ae1ac6650
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-09-28 22:26:02 -0400

    extend IdentityUser

--- ProductStore.DataAccess/Data/ApplicationDbContext.cs
+++ ProductStore.DataAccess/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace ProductStore.DataAccess.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
